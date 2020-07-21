73e0dc2fd32306fe1fbba63d11392e02efbecb00
blake-hodges <tbhodges@gmail.com>
2020-07-21 16:28:11 -0400

    fix error in problem 7 and refactor

--- problem7.js
+++ problem7.js
@@ -7,72 +7,47 @@
 //loop through all numbers
 //check if a number is Prime
 //if it's prime, add to an array
-//check length of array and stop the loop at n
 //return number at array position n
 
 //output the nth prime number
 
-//
-// function nthPrime(n) {
-//     let primesArray = [];
-//     for (let i = 2; i < 10005; i++) {
-//         let result = isPrime(i);
-//         if (result == true) {
-//             primesArray.push(i);
-//         }
-//     }
-//     return primesArray[n];
-//
-//     function isPrime(num) {
-//         let res = true;
-//         if (num == 2 || num == 3) {
-//             return true;
-//         } else if  (num % 2 == 0 && num > 2) {
-//             return false;
-//         }
-//         for (let i = 3; i < Math.sqrt(num); i++) {
-//             if (num % i == 0) {
-//                 res = false
-//             }
-//         }
-//         return res;
-//
-//     }
-//
-// }
+
 
 function nthPrime(n) {
     let primesArray = [];
-    for (let i = 2; i < 105000; i++) {
+    for (let i = 2; primesArray.length <= n; i++) {
         let result = isPrime(i);
         if (result == true) {
             primesArray.push(i);
         }
     }
     return primesArray[n - 1];
-}
 
-function isPrime(num) {
-    let res = true;
-    if (num == 2 || num == 3) {
-        return true;
-    } else if  (num % 2 == 0 && num > 2) {
-        return false;
-    }
-    for (let i = 3; i < num; i++) {
-        if (num % i == 0) {
-            res = false
+    function isPrime(num) {
+        let res = true;
+        if (num == 2 || num == 3) {
+            return true;
+        } else if  (num % 2 == 0) {
+            return false;
         }
-    }
-    return res;
+        for (let i = 3; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                res = false
+            }
+        }
+        return res;
 
+    }
 }
+
+
+
+
 
 
 
 console.log(nthPrime(6));
-
-console.log(nthPrime(13));
+console.log(nthPrime(10));
 console.log(nthPrime(100));
 console.log(nthPrime(1000));
 console.log(nthPrime(10001));
